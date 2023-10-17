b4545b8ac081793a178b0c4bf98d1cf28f122c26
Raghu <draghu222@gmail.com>
2023-10-17 12:53:06 -0500

    Drop Down

---
+++ react-app/src/components/dropdown.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+
+const countries = [
+  {
+    name: "India",
+    value: "IN",
+    cities: ["Delhi", "Mumbai"],
+  },
+  {
+    name: "Pakistan",
+    value: "PK",
+    cities: ["Lahore", "Karachi"],
+  },
+  {
+    name: "USA",
+    value: "US",
+    cities: ["Dallas", "Austin"],
+  },
+];
+
+export default function DropDown() {
+  const [selectedCountry, setSelectedCountry] = useState("");
+  
+  const handleCountryChange = (e) => {
+    setSelectedCountry(e.target.value);
+  };
+
+  return (
+    <div>
+      <select onChange={handleCountryChange} value={selectedCountry}>
+        <option value="">Select a country</option>
+        {countries.map((country) => (
+          <option key={country.value} value={country.value}>
+            {country.name}
+          </option>
+        ))}
+      </select>
+
+      {selectedCountry && (
+        <select>
+          {countries.find((country) => country.value === selectedCountry).cities.map((city, i) => (
+            <option key={i} value={city}>
+              {city}
+            </option>
+          ))}
+        </select>
+      )}
+    </div>
+  );
+}
